fee37b81d0831f40c911e0f3cefcff4b652b6f13
tytleteerachart <tytle.teerachart@gmail.com>
2017-02-09 19:22:22 +0700

    Remove old version of sign-in web application

--- securitysignin/src/main/java/it/kmitl/securitysignin/configuration/SpringSecurityInitializer.java
+++
@@ -1,6 +0,0 @@
-package it.kmitl.securitysignin.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-}
--- securitysignin/src/main/java/it/kmitl/securitysignin/repository/UserAccountDAO.java
+++
@@ -1,8 +0,0 @@
-package it.kmitl.securitysignin.repository;
-
-import it.kmitl.securitysignin.model.UserAccount;
-
-public interface UserAccountDAO {
-
-    public UserAccount getUserAccount(String username);
-}
